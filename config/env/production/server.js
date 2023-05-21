81dd6619d46c6aab0f71b9081188635ad1dd0d6e
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-05-21 14:26:33 +0300

    fix server url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url:  "http://localhost:1337",
+  url:  "http://server.1096137-cd86361.tmweb.ru/",
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
